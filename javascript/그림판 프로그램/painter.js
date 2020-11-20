a65867fa2682197565033a0cb394ce1dc78fe098
rnjsrnrdnjs <49832278+rnjsrnrdnjs@users.noreply.github.com>
2020-11-20 23:45:49 +0900

    Add files via upload

--- javascript/그림판 프로그램/painter.js
+++ javascript/그림판 프로그램/painter.js
@@ -1,12 +1,16 @@
 
-var imgCat=new Image();
+var imgCat=[];
 var imgMouse=new Image();
 var imgCheese=new Image();
 var imgBrush=new Image();
-var canvas;var ctx;
+var canvas;
+var ctx;
 var mouseX=10,mouseY=10;
-
-
+//2차원 배열 초기화 구글링 해보기
+var catYX=[[500,900],[500,900],[500,900],[500,900],[500,900],[500,900],[500,900],[500,900],[500,900],[500,900]];
+var CATNUM=10;
+var dyx=[[0,-1],[-1,0],[0,1],[1,0]];
+var timer;
 //화면을 구성하는요소를 생성하고 요소에 이벤트 리스너를 등록한다.
 function createPainter(parent,width,height){
   var title=elt("h2",null,"치즈의적 고양이");
@@ -26,31 +30,69 @@ function showKey(e){
     else if(e.keyCode==38)d=1;
     else if(e.keyCode==39)d=2;
     else if(e.keyCode==40)d=3;
-    var dyx=[[0,-1],[-1,0],[0,1],[1,0]];
+    ctx.clearRect(mouseX,mouseY,40,40);
     mouseY+=5*dyx[d][0];
     mouseX+=5*dyx[d][1];
-    ctx.clearRect(mouseX-10,mouseY-10,60,60);
+    if(mouseY+40>=600 || mouseX+40>=1000 || mouseY<=0 || mouseX<=0){
+      mouseY+=5*dyx[(d+2)%4][0];
+      mouseX+=5*dyx[(d+2)%4][1];
+    }
     ctx.drawImage(imgMouse,mouseX,mouseY,40,40);
 }
 //무작위 고양이 움직이기
 function randomCatMove(){
+  if(mouseX>=950 && mouseY>=550){
+    alert("승리하셨습니다.")
+    return clearInterval(timer);
+  }
+  for(var i=0;i<CATNUM;i++){
+    // 범위 수정 
+    if(mouseX+40==catYX[i][1] || mouseY+40==catYX[i][0] || mouseX==catYX[i][1]+40 || mouseY==catYX[i][0]+40){
+      alert("패배하셨습니다.")
+      return clearInterval(timer);
+    }
+  }
 
+  for(var i=0;i<CATNUM;i++){
+    var d=Math.floor(Math.random()*4);
+    ctx.clearRect(catYX[i][1],catYX[i][0],40,40);
+    catYX[i][0]+=(3*(i+2))*dyx[d][0];
+    catYX[i][1]+=(3*(i+2))*dyx[d][1];
+    if(catYX[i][0]+40>=550 && catYX[i][1]+40>=950){
+      catYX[i][0]+=(3*(i+2))*dyx[(d+2)%4][0]*2;
+      catYX[i][1]+=(3*(i+2))*dyx[(d+2)%4][1]*2;
+    }
+    else if(catYX[i][0]+40>=600 || catYX[i][1]+40>=1000 || catYX[i][0]<=0 || catYX[i][1]<=0){
+      catYX[i][0]+=(3*(i+1))*dyx[(d+2)%4][0]*2;
+      catYX[i][1]+=(3*(i+1))*dyx[(d+2)%4][1]*2;
+    }
+    ctx.drawImage(imgCat[i],catYX[i][1],catYX[i][0],40,40);
+  }
 }
 
 function createCanvas(canvasWidth,canvasHeight){
   canvas=elt("canvas",{width:canvasWidth,height:canvasHeight});
   ctx=canvas.getContext("2d");
   
-  imgCat.src="cat.jpg";
   imgMouse.src="mouse.jpg";
   imgCheese.src="cheese.jpg";
   imgBrush.src="brush.jpg";
 
   imgMouse.onload=function(){
     ctx.drawImage(imgMouse,10,10,40,40);
-  }
+  };
   imgCheese.onload=function(){
     ctx.drawImage(imgCheese,950,550,40,40)
+  };
+  for(var i=0;i<CATNUM+1;i++){
+    imgCat[i]=new Image();
+    imgCat[i].src="cat.jpg";
+    imgCat[i].style.position="relative";
+  }
+  for(var i=0;i<CATNUM;i++){
+    imgCat[i].onload=function(){
+      ctx.drawImage(imgCat[i],900,500,40,40);
+    };
   }
   canvas.style.border="10px solid gray";
   canvas.style.cursor="pointer";
@@ -185,4 +227,3 @@ controls.alpha=function(ctx){
   },false);
   return label; 
 };
-
