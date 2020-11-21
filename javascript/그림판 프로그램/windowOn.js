1bb5e83fd4eceb70f4111e392ec77e0464c04f58
rnjsrnrdnjs <49832278+rnjsrnrdnjs@users.noreply.github.com>
2020-11-21 13:35:17 +0900

    Update windowOn.js

--- javascript/그림판 프로그램/windowOn.js
+++ javascript/그림판 프로그램/windowOn.js
@@ -1,3 +1,15 @@
-window.onload=function(){
-  createPainter(document.body,800,600);
+window.onload = function () {
+  createPainter(document.body, 1000, 600);
+
+  //초기 맵 그리기 
+  //붓을 가지면 늘어남
+  //붓에따라 그릴수있는양 늘어남
+
+  document.addEventListener("keydown", showKey, false);
+  //게임 끝나는시점 -> 치즈먹으면 끝 승리, 고양이에게 닿으면 끝 패배
+  timer = setInterval(function () {
+    if (isMousedown == false) {
+      randomCatMove();
+    }
+  }, 100);
 };
